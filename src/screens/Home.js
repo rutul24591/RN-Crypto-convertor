7b6371fadea25418f9722dcb34a402c0351756a4
Rutul Amin <rutulamin.ra@gmail.com>
2022-07-25 14:53:32 +0530

    Reflecting correct values, adding loading functionality. App is complete

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,8 @@ import {
   Image, 
   Text, 
   ScrollView,
-  TouchableOpacity 
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
 import { format } from 'date-fns';
 import { Entypo } from '@expo/vector-icons';
@@ -72,19 +73,21 @@ const Home = ({ navigation }) => {
   /** Both of the below lines moved to utils/ConversionContext.js to make use of context  */
   // const [baseCurrency, setBaseCurrency] = useState('USD');
   // const [quoteCurrency, setQuoteCurrency] = useState('GBP');
-  const [value, setValue] = useState('100');
-  
-  const conversionRate = 0.8435;
-  const date = new Date();
+  const [value, setValue] = useState('1');
 
   const {
     baseCurrency,
     quoteCurrency,
     swapCurrencies,
+    date,
+    rates,
+    isLoading
   } = useContext(ConversionContext);
 
+  const conversionRate = rates && rates[quoteCurrency];
+
   // eslint-disable-next-line no-console
-  console.log("Base Currency: ", baseCurrency);
+  console.log("Conversion Rate: ", conversionRate);
 
   /** Below function moved to utils/ConversionContext.js to make use of context  */
   // const swapCurrencies = () => {
@@ -121,46 +124,52 @@ const Home = ({ navigation }) => {
 
           <Text style={styles.textHeader}>Currency Convertor</Text>
 
-          <ConversionInput
-            text={baseCurrency}
-            value={ value }
-            // Drilling down props
-            onButtonPress={() => 
-              navigation.push('CurrencyList', { 
-                title: 'Base Currency', 
-                // activeCurrency : baseCurrency, 
-                // onChange: (currency) => setBaseCurrency(currency),
-                isBaseCurrency: true
-              })
-            }  
-            keyboardType="numeric"
-            // eslint-disable-next-line no-console
-            onChangeText={(text) => setValue(text)}
-          />
-          <ConversionInput
-            text={quoteCurrency}
-            value={ 
-              value && `${(parseFloat(value) * conversionRate).toFixed(2)}`
-            }
-            // Drilling down props
-            onButtonPress={() => 
-              navigation.push('CurrencyList', { 
-                title: 'Quote Currency', 
-                // activeCurrency : quoteCurrency, 
-                // onChange: (currency) => setQuoteCurrency(currency), 
-                isBaseCurrency: false
-              })
-            } 
-            // eslint-disable-next-line no-console
-            onChangeText={(text) => console.log('text', text)}
-            editable={false}
-          />
-          <Text style={styles.text}>
-            {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}`}
-          </Text>
+          { isLoading ? (
+              <ActivityIndicator color={colors.white} size='large'/> 
+            ) : (
+                <>
+                  <ConversionInput
+                    text={baseCurrency}
+                    value={ value }
+                    // Drilling down props
+                    onButtonPress={() => 
+                      navigation.push('CurrencyList', { 
+                        title: 'Base Currency', 
+                        // activeCurrency : baseCurrency, 
+                        // onChange: (currency) => setBaseCurrency(currency),
+                        isBaseCurrency: true
+                      })
+                    }  
+                    keyboardType="numeric"
+                    // eslint-disable-next-line no-console
+                    onChangeText={(text) => setValue(text)}
+                  />
+                  <ConversionInput
+                    text={quoteCurrency}
+                    value={ 
+                      value && `${(parseFloat(value) * conversionRate).toFixed(2)}`
+                    }
+                    // Drilling down props
+                    onButtonPress={() => 
+                      navigation.push('CurrencyList', { 
+                        title: 'Quote Currency', 
+                        // activeCurrency : quoteCurrency, 
+                        // onChange: (currency) => setQuoteCurrency(currency), 
+                        isBaseCurrency: false
+                      })
+                    } 
+                    // eslint-disable-next-line no-console
+                    onChangeText={(text) => console.log('text', text)}
+                    editable={false}
+                  />
+                  <Text style={styles.text}>
+                    {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${date && format(new Date(date), 'MMMM do, yyyy')}`}
+                  </Text>
 
-          <Button text="Reverse currencies" onPress={() => swapCurrencies()}></Button>
-
+                  <Button text="Reverse currencies" onPress={() => swapCurrencies()}></Button>
+                </>
+              )
+          }
           {/* <View style={{ height :  screen.height}}/> */}
           <KeyboardSpacer onToggle={(keyboardIsVisible) => setScrollEnabled(keyboardIsVisible)}/>
         </View>
--- src/utils/ConversionContext.js
+++ src/utils/ConversionContext.js
@@ -1,10 +1,38 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
+import { Alert } from 'react-native';
+import { api } from './api';
 
 export const ConversionContext = createContext();
+const DEFAULT_BASE_CURRENCY = 'USD';
+const DEFAULT_QUOTE_CURRENCY = 'GBP';
 
 export const ConversionContextProvider = ({ children }) => {
-    const [baseCurrency, setBaseCurrency] = useState('USD');
-    const [quoteCurrency, setQuoteCurrency] = useState('GBP');
+    const [baseCurrency, _setBaseCurrency] = useState(DEFAULT_BASE_CURRENCY);
+    const [quoteCurrency, setQuoteCurrency] = useState(DEFAULT_QUOTE_CURRENCY);
+    const [date, setDate] = useState();
+    const [rates, setRates] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    //Override 
+    const setBaseCurrency = async (currency) => {
+        setIsLoading(true);
+        return api(`/latest?base=${currency}`)
+            .then((response) => {
+                // eslint-disable-next-line no-console
+                console.log('Response: ', response);
+                _setBaseCurrency(currency);
+                setDate(response.date);
+                setRates(response.rates);
+            })
+            .catch((error) => {
+                // eslint-disable-next-line no-console
+                console.log('Error: ', error);
+                Alert.alert('Sorry, something went wrong!', error.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
 
     const swapCurrencies = () => {
         setBaseCurrency(quoteCurrency);
@@ -17,8 +45,15 @@ export const ConversionContextProvider = ({ children }) => {
         quoteCurrency,
         swapCurrencies,
         setBaseCurrency,
-        setQuoteCurrency
+        setQuoteCurrency,
+        date,
+        rates,
+        isLoading
     }
+
+    useEffect(() => {
+        setBaseCurrency(DEFAULT_BASE_CURRENCY);
+    },[]);
 
     return(
         <ConversionContext.Provider value={ contextValue }>
